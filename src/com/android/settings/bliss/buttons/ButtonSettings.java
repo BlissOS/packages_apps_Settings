LMODroid/platform_packages_apps_Settings

ButtinSettings: Add "Enable taskbar" toggle
Change-Id: Ie0ea98898c887a70f44511b35ac95f5d2750545d

src/com/android/settings/libremobileos/buttons/ButtonSettings.java
@@ -17,7 +17,9 @@
 
 package com.android.settings.libremobileos.buttons;
 
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVERLAY;
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
 import android.app.ActivityManager;
 import android.content.ComponentName;
@@ -49,6 +51,8 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
+
+import static com.android.systemui.shared.recents.utilities.Utilities.isTablet;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -114,6 +118,8 @@ public class ButtonSettings extends SettingsPreferenceFragment
     private static final String KEY_CLICK_PARTIAL_SCREENSHOT =
             "click_partial_screenshot";
     private static final String KEY_SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+    private static final String KEY_NAV_BAR_INVERSE = "sysui_nav_bar_inverse";
+    private static final String KEY_ENABLE_TASKBAR = "enable_taskbar";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -156,6 +162,8 @@ public class ButtonSettings extends SettingsPreferenceFragment
     private SwitchPreference mTorchLongPressPowerGesture;
     private ListPreference mTorchLongPressPowerTimeout;
     private SwitchPreference mSwapCapacitiveKeys;
+    private SwitchPreference mNavBarInverse;
+    private SwitchPreference mEnableTaskbar;
 
     private PreferenceCategory mNavigationPreferencesCat;
 
@@ -478,6 +486,21 @@ public class ButtonSettings extends SettingsPreferenceFragment
             mSwapCapacitiveKeys.setDependency(KEY_DISABLE_NAV_KEYS);
         }
 
+        mNavBarInverse = findPreference(KEY_NAV_BAR_INVERSE);
+
+        mEnableTaskbar = findPreference(KEY_ENABLE_TASKBAR);
+        if (mEnableTaskbar != null) {
+            if (!hasNavigationBar()) {
+                mNavigationPreferencesCat.removePreference(mEnableTaskbar);
+            } else {
+                mEnableTaskbar.setOnPreferenceChangeListener(this);
+                mEnableTaskbar.setChecked(Settings.System.getInt(getContentResolver(),
+                        Settings.System.ENABLE_TASKBAR,
+                        isTablet(getContext()) ? 1 : 0) == 1);
+                toggleTaskBarDependencies(mEnableTaskbar.isChecked());
+            }
+        }
+
         // Override key actions on Go devices in order to hide any unsupported features
         if (ActivityManager.isLowRamDeviceStatic()) {
             String[] actionEntriesGo = res.getStringArray(R.array.hardware_keys_action_entries_go);
@@ -646,8 +669,58 @@ public class ButtonSettings extends SettingsPreferenceFragment
         } else if (preference == mSwapCapacitiveKeys) {
             mHardware.set(LineageHardwareManager.FEATURE_KEY_SWAP, (Boolean) newValue);
             return true;
+        } else if (preference == mEnableTaskbar) {
+            toggleTaskBarDependencies((Boolean) newValue);
+            if ((Boolean) newValue && is2ButtonNavigationEnabled(getContext())) {
+                // Let's switch to gestural mode if user previously had 2 buttons enabled.
+                setButtonNavigationMode(NAV_BAR_MODE_GESTURAL_OVERLAY);
+            }
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.ENABLE_TASKBAR, ((Boolean) newValue) ? 1 : 0);
+            return true;
         }
         return false;
+    }
+
+    private static boolean is2ButtonNavigationEnabled(Context context) {
+        return NAV_BAR_MODE_2BUTTON == context.getResources().getInteger(
+                com.android.internal.R.integer.config_navBarInteractionMode);
+    }
+
+    private static void setButtonNavigationMode(String overlayPackage) {
+        IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        try {
+            overlayManager.setEnabledExclusiveInCategory(overlayPackage, UserHandle.USER_CURRENT);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    private void toggleTaskBarDependencies(boolean enabled) {
+        if (mNavigationArrowKeys != null) {
+            mNavigationArrowKeys.setEnabled(!enabled);
+        }
+
+        if (mNavBarInverse != null) {
+            mNavBarInverse.setEnabled(!enabled);
+        }
+
+        if (mNavigationBackLongPressAction != null) {
+            mNavigationBackLongPressAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationHomeLongPressAction != null) {
+            mNavigationHomeLongPressAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationHomeDoubleTapAction != null) {
+            mNavigationHomeDoubleTapAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationAppSwitchLongPressAction != null) {
+            mNavigationAppSwitchLongPressAction.setEnabled(!enabled);
+        }
     }
 
     private static void writeDisableNavkeysOption(Context context, boolean enabled) {
@@ -829,14 +902,7 @@ public class ButtonSettings extends SettingsPreferenceFragment
             mDisableNavigationKeys.setEnabled(false);
             mNavigationPreferencesCat.setEnabled(false);
             if (!mDisableNavigationKeys.isChecked()) {
-                IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(
-                        ServiceManager.getService(Context.OVERLAY_SERVICE));
-                try {
-                    overlayManager.setEnabledExclusiveInCategory(NAV_BAR_MODE_3BUTTON_OVERLAY,
-                            UserHandle.USER_CURRENT);
-                } catch (RemoteException e) {
-                    throw e.rethrowFromSystemServer();
-                }
+                setButtonNavigationMode(NAV_BAR_MODE_3BUTTON_OVERLAY);
             }
             writeDisableNavkeysOption(getActivity(), mDisableNavigationKeys.isChecked());
             updateDisableNavkeysOption();

BlissOS/packages_apps_Settings

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

src/com/android/settings/bliss/buttons/ButtonSettings.java
@@ -17,7 +17,9 @@
 
 package com.android.settings.bliss.buttons;
 
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVERLAY;
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
 import android.app.ActivityManager;
 import android.content.ComponentName;
@@ -49,6 +51,8 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreferenceCompat;
+
+import static com.android.systemui.shared.recents.utilities.Utilities.isTablet;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -114,6 +118,8 @@ public class ButtonSettings extends SettingsPreferenceFragment
     private static final String KEY_CLICK_PARTIAL_SCREENSHOT =
             "click_partial_screenshot";
     private static final String KEY_SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+    private static final String KEY_NAV_BAR_INVERSE = "sysui_nav_bar_inverse";
+    private static final String KEY_ENABLE_TASKBAR = "enable_taskbar";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -156,6 +162,8 @@ public class ButtonSettings extends SettingsPreferenceFragment
     private SwitchPreferenceCompat mTorchLongPressPowerGesture;
     private ListPreference mTorchLongPressPowerTimeout;
     private SwitchPreferenceCompat mSwapCapacitiveKeys;
+    private SwitchPreferenceCompat mNavBarInverse;
+    private SwitchPreferenceCompat mEnableTaskbar;
 
     private PreferenceCategory mNavigationPreferencesCat;
 
@@ -478,6 +486,21 @@ public class ButtonSettings extends SettingsPreferenceFragment
             mSwapCapacitiveKeys.setDependency(KEY_DISABLE_NAV_KEYS);
         }
 
+        mNavBarInverse = findPreference(KEY_NAV_BAR_INVERSE);
+
+        mEnableTaskbar = findPreference(KEY_ENABLE_TASKBAR);
+        if (mEnableTaskbar != null) {
+            if (!hasNavigationBar()) {
+                mNavigationPreferencesCat.removePreference(mEnableTaskbar);
+            } else {
+                mEnableTaskbar.setOnPreferenceChangeListener(this);
+                mEnableTaskbar.setChecked(Settings.System.getInt(getContentResolver(),
+                        Settings.System.ENABLE_TASKBAR,
+                        isTablet(getContext()) ? 1 : 0) == 1);
+                toggleTaskBarDependencies(mEnableTaskbar.isChecked());
+            }
+        }
+
         // Override key actions on Go devices in order to hide any unsupported features
         if (ActivityManager.isLowRamDeviceStatic()) {
             String[] actionEntriesGo = res.getStringArray(R.array.hardware_keys_action_entries_go);
@@ -646,8 +669,58 @@ public class ButtonSettings extends SettingsPreferenceFragment
         } else if (preference == mSwapCapacitiveKeys) {
             mHardware.set(LineageHardwareManager.FEATURE_KEY_SWAP, (Boolean) newValue);
             return true;
+        } else if (preference == mEnableTaskbar) {
+            toggleTaskBarDependencies((Boolean) newValue);
+            if ((Boolean) newValue && is2ButtonNavigationEnabled(getContext())) {
+                // Let's switch to gestural mode if user previously had 2 buttons enabled.
+                setButtonNavigationMode(NAV_BAR_MODE_GESTURAL_OVERLAY);
+            }
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.ENABLE_TASKBAR, ((Boolean) newValue) ? 1 : 0);
+            return true;
         }
         return false;
+    }
+
+    private static boolean is2ButtonNavigationEnabled(Context context) {
+        return NAV_BAR_MODE_2BUTTON == context.getResources().getInteger(
+                com.android.internal.R.integer.config_navBarInteractionMode);
+    }
+
+    private static void setButtonNavigationMode(String overlayPackage) {
+        IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        try {
+            overlayManager.setEnabledExclusiveInCategory(overlayPackage, UserHandle.USER_CURRENT);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    private void toggleTaskBarDependencies(boolean enabled) {
+        if (mNavigationArrowKeys != null) {
+            mNavigationArrowKeys.setEnabled(!enabled);
+        }
+
+        if (mNavBarInverse != null) {
+            mNavBarInverse.setEnabled(!enabled);
+        }
+
+        if (mNavigationBackLongPressAction != null) {
+            mNavigationBackLongPressAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationHomeLongPressAction != null) {
+            mNavigationHomeLongPressAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationHomeDoubleTapAction != null) {
+            mNavigationHomeDoubleTapAction.setEnabled(!enabled);
+        }
+
+        if (mNavigationAppSwitchLongPressAction != null) {
+            mNavigationAppSwitchLongPressAction.setEnabled(!enabled);
+        }
     }
 
     private static void writeDisableNavkeysOption(Context context, boolean enabled) {
@@ -829,14 +902,7 @@ public class ButtonSettings extends SettingsPreferenceFragment
             mDisableNavigationKeys.setEnabled(false);
             mNavigationPreferencesCat.setEnabled(false);
             if (!mDisableNavigationKeys.isChecked()) {
-                IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(
-                        ServiceManager.getService(Context.OVERLAY_SERVICE));
-                try {
-                    overlayManager.setEnabledExclusiveInCategory(NAV_BAR_MODE_3BUTTON_OVERLAY,
-                            UserHandle.USER_CURRENT);
-                } catch (RemoteException e) {
-                    throw e.rethrowFromSystemServer();
-                }
+                setButtonNavigationMode(NAV_BAR_MODE_3BUTTON_OVERLAY);
             }
             writeDisableNavkeysOption(getActivity(), mDisableNavigationKeys.isChecked());
             updateDisableNavkeysOption();